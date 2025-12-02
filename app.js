216ad325a9858c851ce13634f0263274a3326797
Muhammadhamza1261 <ameeralbaloshi377@gmail.com>
2025-12-02 09:01:48 +0500

    PULL

---
+++ app.js
@@ -0,0 +1,142 @@
+// chapter 01--- (Q1)
+
+alert("welcome to my page")
+
+// chapter 01--- (Q2)
+
+alert("Error! please enter a valid password")
+
+//chapter 01--- (Q3)
+
+alert("Welcome to js land... \n Happy coding!")
+
+//chapter 01--- (Q4)
+
+alert("Welcome to js land!")
+alert("happyc coding")
+
+//chapter 01--- (Q5)
+console.log(alert("Hey! i can run js through in my my browser cosole."))
+
+//chapter 01--- (Q6)
+
+alert("Hey im using alerts in my js assigment.")
+
+//chapter 01--- (Q7)
+
+alert(" Q7 answer go to my index.html page and check")
+
+// ----------chappter 2 start---------
+
+//chapter 2 (Q1)
+
+var userName
+
+//chapter 2 (Q2)
+
+var myName= "Hamza";
+
+var firstName= "Ameer";
+var lastName= "Hamza"
+var fullName= firstName +lastName;
+console.log(fullName);
+
+//chapter 2 (Q3)
+
+var message;
+message="Hello world";
+alert(message);
+ 
+//chapter 2 (Q4)
+
+var nam="Hamza"
+var age= "21"
+var certification="certified mobile app developer"
+ 
+alert("Hamza")
+alert("21")
+alert("certified mobile app developer")
+
+//chapter 2 (Q5)
+
+alert("pizza \n pizz \n piz \n pi \n p")
+
+//chapter 2 (Q6)
+
+var myemAil= "xyz@gmail.com"
+alert("my gmail is : " + myemAil )
+
+//chapter 2 (Q7)
+
+var book= "A smater way to learn javascript"
+alert("Iam  trying to learn from "  + book)
+
+//chapter 2 (Q8)
+
+document.write("yah i can write html contant from java")
+
+
+//chapter 2 (Q9)
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// _____________-Chapter 3 _____________
+
+//chapter 3 (Q1)
+
+var age="I am 21 year old."
+alert(age) 
+
+//chapter 3 (Q2)
+
+var viste="14 time."
+alert("You have visited this site" + viste)
+
+//chapter 3 (Q4)
+
+var birthyear= "2004."
+alert("my birth year is : " + birthyear )
+
+
+//chapter 3 (Q5)
+ 
+var userName="Hamza"
+var productName="shirts"
+var quantity="5"
+
+document.write (userName + " ordered " + quantity + " " + productName + "from clothing store")
+
+// ---------------chapter 4------------------------
+
+//chapter 4 (Q1)
+
+var username="hamza"; var age=21; var learning="script";
+
+//chapter 4 (Q2)
+
+//---legal---
+
+var userName
+var $username
+var USERNAME
+var username1
+var user_name
+
+//----illegal----
+
+//  var 1userName   // start with  num
+//  var user name     // including space
+//  var USENAME="HAMZA" //case sensitive
+//  var username not same 
+//  console.log(USERNAME)
+//  console.log(username)
+//  var user@name ="hazza" //using  other Symbol $ included 
+
+//chapter 4 (Q3)
+
+document.write("<h2>Ruling for js varible</h2>")
+
+document.write("variable name can  only containe,  numbers , $ and _. for Example $my_1stVariable <br>")
+document.write("variable must begin with a letter , $ or _ . For Example : $name ,_name or name <br>")
+document.write("variable names are csse sensitive.<br>")
+document.write("variables names should not be JS keywords.")
